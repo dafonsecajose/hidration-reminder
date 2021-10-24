6d95bc4173712efe5d577f82b9f87ae51057ac19
José D'Afonseca <josepfj1@gmail.com>
2021-10-24 15:06:45 -0300

    first commit with layout fragments

---
+++ app/src/main/java/br/com/jose/hidratereminder/ui/hidrate/HidrateFragment.kt
@@ -0,0 +1,41 @@
+package br.com.jose.hidratereminder.ui.hidrate
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import br.com.jose.hidratereminder.databinding.FragmentHidrateBinding
+
+class HidrateFragment : Fragment() {
+
+    private lateinit var homeViewModel: HidrateViewModel
+    private var _binding: FragmentHidrateBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HidrateViewModel::class.java)
+
+        _binding = FragmentHidrateBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
