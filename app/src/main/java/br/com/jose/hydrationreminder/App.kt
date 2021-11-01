42e205f2b9d6526dd4ad8f47b2cf4ede72c468bf
José D'Afonseca <josepfj1@gmail.com>
2021-11-01 16:06:39 -0300

    configure receiver  in manifest

--- app/src/main/java/br/com/jose/hydrationreminder/App.kt
+++ app/src/main/java/br/com/jose/hydrationreminder/App.kt
@@ -3,6 +3,7 @@ package br.com.jose.hydrationreminder
 import android.app.Application
 import br.com.jose.hydrationreminder.data.di.DataModules
 import br.com.jose.hydrationreminder.domain.di.DomainModules
+import br.com.jose.hydrationreminder.notifications.di.NotificationModule
 import br.com.jose.hydrationreminder.presentation.di.PresentationModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -16,6 +17,7 @@ class App: Application() {
         }
         DataModules.load()
         DomainModules.load()
+        NotificationModule.load()
         PresentationModules.load()
     }
 
