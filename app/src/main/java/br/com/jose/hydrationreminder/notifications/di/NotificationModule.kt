d4663b5c97f56c6719b372c664dc28e613961c0e
José D'Afonseca <josepfj1@gmail.com>
2021-11-01 16:04:53 -0300

    notification module

---
+++ app/src/main/java/br/com/jose/hydrationreminder/notifications/di/NotificationModule.kt
@@ -0,0 +1,19 @@
+package br.com.jose.hydrationreminder.notifications.di
+
+import br.com.jose.hydrationreminder.notifications.NotificationsSchedule
+import org.koin.android.ext.koin.androidApplication
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+object NotificationModule {
+    fun load() {
+        loadKoinModules(notificationScheduleModule())
+    }
+
+    private fun notificationScheduleModule(): Module {
+        return module {
+            factory { NotificationsSchedule(androidApplication()) }
+        }
+    }
+}
